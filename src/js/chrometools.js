710ed98011d7febab6ed23b422395b989bad030a
Simon Weber <simon@simonmweber.com>
2016-03-13 22:18:15 -0400

    open a new tab every time for page action [#48]

--- src/js/chrometools.js
+++ src/js/chrometools.js
@@ -23,14 +23,17 @@ function unlessError(func) {
 exports.unlessError = unlessError;
 
 exports.focusOrCreateExtensionTab = function focusOrCreateExtensionTab(url) {
-  chrome.tabs.query({url}, unlessError(tabs => {
-    console.info('tab query for', url, 'yields', JSON.stringify(tabs, null, '\t'));
-    if (tabs.length) {
-      chrome.tabs.update(tabs[0].id, {active: true}, unlessError(t => console.log('selected', t)));
-    } else {
-      chrome.tabs.create({url, active: true}, unlessError(t => console.log('created', t)));
-    }
-  }));
+  chrome.tabs.create({url, active: true}, unlessError(t => console.log('created', t)));
+
+  // There seems to be a bug in Chrome preventing tabs.query from working as expected.
+  // chrome.tabs.query({url}, unlessError(tabs => {
+  //   console.info('tab query for', url, 'yields', JSON.stringify(tabs, null, '\t'));
+  //   if (tabs.length) {
+  //     chrome.tabs.update(tabs[0].id, {active: true}, unlessError(t => console.log('selected', t)));
+  //   } else {
+  //     chrome.tabs.create({url, active: true}, unlessError(t => console.log('created', t)));
+  //   }
+  // }));
 };
 
 exports.goToManager = function goToManager(userId) {
