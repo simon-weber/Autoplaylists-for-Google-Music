f246c8d9de6ca2f3af669ceba86f59bfa9c2d117
Hugo Haas <hugoh@hugoh.net>
2017-02-19 20:50:23 -0800

    Using the local (node_modules) watchify

--- watch.sh
+++ watch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for f in src/js/*.js; do
-    watchify "$f" -d -o "src/js-built/$(basename $f)" -v &
+    node_modules/.bin/watchify "$f" -d -o "src/js-built/$(basename $f)" -v &
 done
 
 wait $!
